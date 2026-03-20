93c881b2dae56d34e29fba1ff3631ab85a6c3107
nataries <natalizastavnaya@icloud.com>
2026-03-20 11:12:08 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -5,6 +5,54 @@ console.log(a2 - b2);
 console.log(a2 * b2);
 console.log(a2 / b2);
 
+let yourAge = 18;
+if (yourAge >=18) {
+    console.log("Доступ разрешён");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let temperature = 6;
+if (temperature < 0) {
+    console.log("Холодно");
+} else if (temperature>=0 && temperature <=20) {
+    console.log("Прохладно");
+} else {
+    console.log("Тепло");
+}
+
+let isLoggedIn = true;
+let isAdmin = true;
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченный доступ");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let a3 = 10;
+let b3 = "10";
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let monthNumber;
+switch(monthNumber) {
+    case 1: console.log("Январь"); break;
+    case 2: console.log("Февраль"); break;
+    case 3: console.log("Март"); break;
+    case 4: console.log("Апрель"); break;
+    case 5: console.log("Май"); break;
+    case 6: console.log("Июнь"); break;
+    case 7: console.log("Июль"); break;
+    case 8: console.log("Август"); break;
+    case 9: console.log("Сентябрь"); break;
+    case 10: console.log("Октябрь"); break;
+    case 11: console.log("Ноябрь"); break;
+    case 12: console.log("Декабрь"); break;
+    default: console.log("Такого месяца не существует!");
+}
+
 // console.log("Hello, JavaScript!");
 
 // let age = 20;
